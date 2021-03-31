8e931cd66017c29b5092083a0343ee117d6c0b3e
Satyam Singh <trueleo@protonmail.com>
2021-03-31 10:18:58 +0530

    use os.environ

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, InputMediaPhoto, InputMediaVideo
 import database
+import os
 import asyncio
 import shortuuid
 from collections import defaultdict
