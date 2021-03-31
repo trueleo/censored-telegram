53ecb1946e1105503f24753dfadd7e94ca5be62f
Satyam Singh <trueleo@protonmail.com>
2021-03-31 10:57:28 +0530

    hm

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ BOT_TOKEN = os.environ.get('BOTKEY')
 API_ID = os.environ.get('API_ID')
 API_HASH = os.environ.get('API_HASH')
 
-app = Client(api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client(session_name=':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 uuid_media = dict()
 group_uuid = dict()
